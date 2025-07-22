d2ffa4c319e6e0361ffcb8561ba5b5cbb12b4563
Tanishka0009 <tanishkasr@gmail.com>
2025-07-22 20:23:39 +0530

    Add files via upload

--- a2.py
+++ a2.py
@@ -1,5 +1,7 @@
+print("Value starts from 1")
 value=int(input("Enter the number:"))
-if value%2 ==0 :
-    print("Even number")
-else:
-        print("Odd number")
+a=0
+for number in range(1, value+1):
+    a+=number
+print("The sum of the numbers are " + str(a))
+
